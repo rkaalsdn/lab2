706ba3b23ab608d871563c3b5d76f8602a43cc5a
Yoonsung Ahn <yyssahn@hotmail.com>
2013-01-20 22:43:00 -0800

    back to original

--- src/ca/ubc/cpsc310/gitlab/client/GitLab.java
+++ src/ca/ubc/cpsc310/gitlab/client/GitLab.java
@@ -53,38 +53,8 @@ public class GitLab implements EntryPoint {
 	 * Used to display users 
 	 * @param users
 	 */
-	public void displayUsers(List<IUser> users)
+	public void displayUsers()
 	{
 
-		RootPanel.get("root").add(flexTable);
-		
-		flexTable.setText(0,0, "Name");
-		
-		flexTable.setText(0,1,"Language");
-		flexTable.setText(0,2, "Shopping Cart Size");
-		flexTable.setText(0,3, "Wish List Size");
-		flexTable.setStyleName("centered-table", true);
-		
-		for(int i=0; i < users.size(); i++)
-		{
-		
-			IUser user = users.get(i);
-			
-			flexTable.setText(i+1,0,user.getName());
-			if(user.getLanguage().trim().equals("EN"))
-			{
-				flexTable.setWidget(i+1, 1, new Image("uk.png"));
-			} else if (user.getLanguage().trim().equals("FR"))
-			{
-				flexTable.setWidget(i+1, 1, new Image("fr.png"));
-			} else
-			{
-				flexTable.setText(i+1,1,user.getLanguage());
-			}
-			
-			flexTable.setText(i+1,2,String.valueOf(user.getShoppingCart().size()));
-			
-			flexTable.setText(i+1,3,String.valueOf(user.getWishList().size()));
-		}
 	}
 }
